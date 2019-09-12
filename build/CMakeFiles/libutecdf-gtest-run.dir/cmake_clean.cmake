a895d22834a790b5e091447ce98ed6b914fd940f
Alexander <aocsa.cs@gmail.com>
2019-09-12 18:32:35 -0500

    update ctest

--- build/CMakeFiles/libutecdf-gtest-run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libutecdf-gtest-run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libutecdf-gtest-run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(utecdf-gtest "/home/aocsa/CLionProjects/utecdf_cpp/build/utecdf-gtest")
+set_tests_properties(utecdf-gtest PROPERTIES  _BACKTRACE_TRIPLES "/home/aocsa/CLionProjects/utecdf_cpp/CMakeLists.txt;86;add_test;/home/aocsa/CLionProjects/utecdf_cpp/CMakeLists.txt;97;blazingdb_artifact;/home/aocsa/CLionProjects/utecdf_cpp/CMakeLists.txt;0;")
